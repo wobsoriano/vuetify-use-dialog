4340733477efe63315885a42a2cf736b63f0fc74
wobsoriano <sorianorobertc@gmail.com>
2023-12-14 22:50:41 -0800

    bump deps

--- build.config.ts
+++ build.config.ts
@@ -2,9 +2,11 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: [
-    { builder: 'mkdist', input: './src/' },
+    { builder: 'mkdist', input: './src', pattern: ['**/*.vue'], loaders: ['vue'] },
+    { builder: 'mkdist', input: './src', pattern: ['**/*.ts'], format: 'cjs', loaders: ['js'] },
+    { builder: 'mkdist', input: './src', pattern: ['**/*.ts'], format: 'esm', loaders: ['js'] },
   ],
   declaration: true,
   clean: true,
-  externals: ['vuetify', 'vuetify/components'],
+  externals: ['vue', 'vuetify', 'vuetify/components'],
 })
